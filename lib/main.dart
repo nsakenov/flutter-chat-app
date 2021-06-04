fbce2e435e036b0a23197655ea529616d81d1107
nurcity <nurbol.sakenov@outlook.com>
2021-06-04 22:46:25 +0200

    debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
           textTheme: TextTheme(
             body1: TextStyle(color: Colors.black54),
